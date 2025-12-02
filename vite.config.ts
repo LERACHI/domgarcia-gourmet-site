debe89e063b291dca5f2d63caba67171043fd3a2
LERACHI <leandrorachi@gmail.com>
2025-12-02 08:58:56 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/domgarcia-gourmet-site/",
+  server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
+
